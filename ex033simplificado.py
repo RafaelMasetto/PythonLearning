ea4b35b6cc65c6f00cedef2e465ff8250ebdce6b
RafaelMasetto <127772302+RafaelMasetto@users.noreply.github.com>
2023-03-15 19:24:39 -0300

    Add files via upload

---
+++ ex033simplificado.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+lista = [n1, n2, n3]
+lista.sort()
+print(f'O menor número é {lista[0]} e o maior número é {lista[2]}')
